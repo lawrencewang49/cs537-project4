246d51ca51f49e68cbaacaf5dc9a341fa760f5d4
mwatiker <95242330+mwatiker@users.noreply.github.com>
2024-03-08 06:57:00 -0800

    Basic wunmap, getpgdirinfo, and getwmapinfo implementation

--- xv6-public/wmap.c
+++ xv6-public/wmap.c
@@ -35,7 +35,25 @@ uint wmap(uint addr, int length, int flags, int fd) {
 }
 
 int wunmap(uint addr) {
-
+    int found = 0;
+    for (int i = 0; i < num_mappings; i++) {
+        if (mappings[i].addr == addr) {
+            found = 1;
+            // Shift all subsequent mappings one position towards the start
+            for (int j = i; j < num_mappings - 1; j++) {
+                mappings[j] = mappings[j + 1];
+            }
+            num_mappings--; // Decrease the total number of mappings
+            break; // Exit the loop once the mapping is found and handled
+        }
+    }
+    
+    if (!found) {
+        // No mapping found for the given address
+        return FAILED;
+    }
+    
+    return SUCCESS;
 }
 
 uint wremap(uint oldaddr, int oldsize, int newsize, int flags) {
@@ -43,9 +61,29 @@ uint wremap(uint oldaddr, int oldsize, int newsize, int flags) {
 }
 
 int getpgdirinfo(struct pgdirinfo *pdinfo) {
-    
+    struct proc *curproc = myproc();
+    pdinfo->n_upages = 0;
+    for (int i = 0; i < MAX_UPAGE_INFO && i < curproc->sz; i += PGSIZE) {
+        // If the page is a user page
+        if (curproc->pgdir[i / PGSIZE] & PTE_U) { 
+            // Store the virtual and physical addresses of the page
+            pdinfo->va[pdinfo->n_upages] = i;
+            pdinfo->pa[pdinfo->n_upages] = PTE_ADDR(curproc->pgdir[i / PGSIZE]);
+            pdinfo->n_upages++;
+        }
+    }
+    return 0;
 }
 
 int getwmapinfo(struct wmapinfo *wminfo) {
-
+    struct proc *curproc = myproc();
+    wminfo->total_mmaps = curproc->total_mmaps;
+    for (int i = 0; i < wminfo->total_mmaps; i++) {
+        // Store the address, length, and number of loaded pages for each memory map
+        wminfo->addr[i] = curproc->wmaps[i].addr;
+        wminfo->length[i] = curproc->wmaps[i].length;
+        wminfo->n_loaded_pages[i] = curproc->wmaps[i].n_loaded_pages;
+    }
+    return 0;
 }
+
